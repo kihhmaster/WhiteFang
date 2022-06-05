eb1c2b3485ff07770c928f99c9ffd7baf4cfd36f
kihhmaster <kihhmaster@yandex.ru>
2022-06-05 18:23:19 +0300

    Работаю над news

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,26 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import '~/node_modules/jquery/dist/jquery.min.js'
+import '~/node_modules/zurb-twentytwenty/js/jquery.event.move.js'
+import '~/node_modules/zurb-twentytwenty/js/jquery.twentytwenty.js'
 import { Swiper, Pagination, Scrollbar, Navigation, Controller, Autoplay, Mousewheel, Parallax, EffectFade, EffectCoverflow, Thumbs} from 'swiper'
 Swiper.use([ Pagination, Scrollbar, Navigation, Controller, Autoplay, Mousewheel, Parallax, EffectFade, EffectCoverflow, Thumbs])
 document.addEventListener('DOMContentLoaded', () => {
+	$(function(){
+		$("#container1").twentytwenty();
+	});
+	$(function(){
+		$(".twentytwenty-container").twentytwenty({
+			default_offset_pct: 0.7, // How much of the before image is visible when the page loads
+			// orientation: 'vertical', // Orientation of the before and after images ('horizontal' or 'vertical')
+			// before_label: 'January 2017', // Set a custom before label
+			// after_label: 'March 2017', // Set a custom after label
+			no_overlay: true, //Do not show the overlay with before and after
+			// move_slider_on_hover: true, // Move slider on mouse hover?
+			move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+			click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+		});
+	});
 	const sliderAnimals = new Swiper(".slider__animals", {
     spaceBetween: 0,
     slidesPerView: 3,
@@ -16,6 +34,16 @@ document.addEventListener('DOMContentLoaded', () => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
     }
+  });
+	const sliderRooms = new Swiper(".slider__rooms", {
+    spaceBetween: 0,
+    slidesPerView: 1,
+		speed: 2000,
+    centeredSlides: true,
+		pagination: {
+			el: '.swiper-pagination__rooms',
+			type: 'bullets',
+		},
   });
 	const sliderAdvantages = new Swiper(".slider__advantages", {
 		spaceBetween: 30,
